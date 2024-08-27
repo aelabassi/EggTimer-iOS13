892a1cb8419accc3f743384cb8981a7e83e51c96
aelabassi <abderrazaq.elabassi@uit.ac.ma>
2024-08-27 12:19:35 +0100

    simple time for cooking eggs

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -8,23 +8,74 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-    
-    let softTime = 4
-    let mediumTime = 8
-    let hardTime = 16
-    @IBAction func hardenessSelected(_ sender: UIButton) {
-        let hardness = sender.currentTitle!
-        
-        if (hardness == "Hard"){
-            print(hardTime)
-        }else if (hardness == "Medium"){
-            print(mediumTime)
-        }else{
-            print(softTime)
+class CountdownTimer {
+    var timer: Timer?
+    private var _secondsRemaining: Int
+
+        var remainingSeconds: Int {
+            get {
+                return _secondsRemaining
+            }
+            set {
+                _secondsRemaining = newValue
+                // Restart the timer with the new remaining seconds
+                timer?.invalidate()
+                start()
+            }
         }
     
+    init(seconds: Int){
+        self._secondsRemaining = seconds
+    }
+    
+    func start(){
+        timer = Timer.scheduledTimer(withTimeInterval: 1.0, repeats: true){[weak self] timer in
+            guard let self = self else {return}
+            
+            if self.remainingSeconds > 0 {
+                self._secondsRemaining -= 1
+                print("seconds remaining \(self.remainingSeconds) seconds")
+            }else{
+                self.timer?.invalidate()
+                print("Countdown finished")
+            }
+        }
+    }
+}
+
+class ViewController: UIViewController {
+    
+    
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    let eggTimer = ["Soft": 300, "Medium": 320, "Hard": 720]
+    var secondsPassed = 0
+    var totalTime = 0
+    var timer = Timer()
+    
+    @IBAction func hardenessSelected(_ sender: UIButton) {
+        timer.invalidate()
+        let hardness = sender.currentTitle!
+        totalTime = eggTimer[hardness]!
+        secondsPassed = 0
+        progressView.progress = 0.0
+        titleLabel.text = hardness
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil ,repeats: true)
         
+        
+    
+        
+    }
+    
+    @objc func updateTimer(){
+        if secondsPassed <= totalTime {
+            progressView.progress = Float(secondsPassed)/Float(totalTime)
+            secondsPassed += 1
+        }else{
+            timer.invalidate()
+            titleLabel.text = "DONE !!"
+        }
     }
     
 }
